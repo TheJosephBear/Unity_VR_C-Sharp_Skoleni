c048fadb1346d7d881dcebd021edd53a25522dec
PalacinkaVelika <128801641+TheJosephBear@users.noreply.github.com>
2025-11-27 13:01:22 +0100

    magic added

--- 8-VRProject/VRTest/Assets/_Features/BasicOvladani/InputReading.cs
+++ 8-VRProject/VRTest/Assets/_Features/BasicOvladani/InputReading.cs
@@ -3,6 +3,9 @@ using UnityEngine.InputSystem;
 
 public class InputReading : MonoBehaviour {
 
+    public ParticleSystem MagicL;
+    public ParticleSystem MagicR;
+
     public InputActionProperty InputTriggerL;
     public InputActionProperty InputTriggerR;
     public InputActionProperty InputGrabL;
@@ -20,5 +23,12 @@ public class InputReading : MonoBehaviour {
         print($"Trigger R: {InputTriggerR.action.ReadValue<float>()}");
         print($"Grab L: {InputGrabL.action.ReadValue<float>()}");
         print($"Grab R: {InputGrabR.action.ReadValue<float>()}");
+
+        if(InputTriggerL.action.ReadValue<float>() == 1f) {
+            MagicL.Play();
+        }
+        if (InputTriggerR.action.ReadValue<float>() == 1f) {
+            MagicR.Play();
+        }
     }
 }
