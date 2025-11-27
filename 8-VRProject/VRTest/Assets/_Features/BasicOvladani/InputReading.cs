d0fc7675998e3cdf9034a4aa976e44cc24bd99bd
PalacinkaVelika <128801641+TheJosephBear@users.noreply.github.com>
2025-11-27 12:33:22 +0100

    input reading

---
+++ 8-VRProject/VRTest/Assets/_Features/BasicOvladani/InputReading.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputReading : MonoBehaviour {
+
+    public InputActionProperty InputTriggerL;
+    public InputActionProperty InputTriggerR;
+    public InputActionProperty InputGrabL;
+    public InputActionProperty InputGrabR;
+
+    void Update() {
+        print($"Trigger L: {InputTriggerL.action.ReadValue<float>()}");
+        print($"Trigger R: {InputTriggerR.action.ReadValue<float>()}");
+        print($"Grab L: {InputGrabL.action.ReadValue<float>()}");
+        print($"Grab R: {InputGrabR.action.ReadValue<float>()}");
+    }
+}
