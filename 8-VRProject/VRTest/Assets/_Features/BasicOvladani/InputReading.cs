54b6002368b3af585ebbd3bda3b0f50ec8e119bd
Pepa Bůh <128801641+TheJosephBear@users.noreply.github.com>
2025-11-27 12:55:34 +0100

    enabling input

--- 8-VRProject/VRTest/Assets/_Features/BasicOvladani/InputReading.cs
+++ 8-VRProject/VRTest/Assets/_Features/BasicOvladani/InputReading.cs
@@ -8,6 +8,13 @@ public class InputReading : MonoBehaviour {
     public InputActionProperty InputGrabL;
     public InputActionProperty InputGrabR;
 
+    private void Start() {
+        InputTriggerL.action.Enable();
+        InputTriggerR.action.Enable();
+        InputGrabL.action.Enable();
+        InputGrabR.action.Enable();
+    }
+
     void Update() {
         print($"Trigger L: {InputTriggerL.action.ReadValue<float>()}");
         print($"Trigger R: {InputTriggerR.action.ReadValue<float>()}");
