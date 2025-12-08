af966784ddd67e9405804c81877ed881ca1a058b
Joseph Bear <128801641+TheJosephBear@users.noreply.github.com>
2025-12-08 16:19:29 +0100

    added ui + feedback for testing

---
+++ 8-VRProject/VRTest/Assets/_Features/BasicOvladani/HapticInvoker.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Haptics;
+using UnityEngine.XR.OpenXR.Input;
+
+public class HapticInvoker : MonoBehaviour {
+
+    [Range(0f, 10f)]
+    public float Amplitude = 0.7f;
+    [Range(0f, 2f)]
+    float Duration = 0.2f;
+    [Range(0f, 10f)]
+    float Frequency = 0.2f;
+
+    public HapticImpulsePlayer HapticLeft;
+    public HapticImpulsePlayer HapticRight;
+    public InputActionReference TriggerLeft;
+    public InputActionReference TriggerRight;
+
+    public void TriggerHapticLeft() {
+        HapticLeft.SendHapticImpulse(Amplitude, Duration, Frequency);
+    }
+
+    public void TriggerHapticRight() {
+        HapticRight.SendHapticImpulse(Amplitude, Duration, Frequency);
+    }
+
+    void OnLeftTrigger(InputAction.CallbackContext ctx) {
+        TriggerHapticLeft();
+    }
+
+    void OnRightTrigger(InputAction.CallbackContext ctx) {
+        TriggerHapticRight();
+    }
+
+    void OnEnable() {
+        TriggerLeft.action.Enable();
+        TriggerRight.action.Enable();
+
+        TriggerLeft.action.performed += OnLeftTrigger;
+        TriggerRight.action.performed += OnRightTrigger;
+    }
+
+}
